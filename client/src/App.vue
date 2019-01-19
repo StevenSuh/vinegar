7dbe66a40e09d086e00ecd833f49bf64fee2a7ee
Jeffrey Yang <jeffreyyang@Jeffreys-MacBook-Air.local>
2019-01-18 22:36:37 -0800

    vue routing

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="wadup coolman chat app"></HelloWorld>
-    <editorTest></editorTest>
+   <!-- <HelloWorld msg="wadup coolman chat app"></HelloWorld>
+   <editorTest></editorTest> -->
+   <router-link to="editor">Editor</router-link>
+   <router-view/>
   </div>
 </template>
 
---
+++ client/src/views/introPage.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="intro">
+    <h1>this is the landing page!!!!!!!</h1>
+    <input type="text" id="roomName" value="somethingnotblank">
+    <button id="enterRoom">
+        enter
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'introPage',
+  mounted: function(){
+      let button = document.getElementById("enterRoom")
+      let roomName = document.getElementById("roomName")
+      button.addEventListener("click", ()=>{
+          this.$router.push(`editor/${roomName.value}`)
+          
+
+      })
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
