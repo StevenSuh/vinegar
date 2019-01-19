99f423c1fda460e5a696710c67a98592c1e5e00b
Jeffrey Yang <jeffreyyang@Jeffreys-MacBook-Air.local>
2019-01-18 21:31:24 -0800

    initial front end files

---
+++ client/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="wadup coolman chat app"></HelloWorld>
+    <editorTest></editorTest>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import editorTest from './components/editor.vue'
+
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+
+      editorTest
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/editor.vue
@@ -0,0 +1,222 @@
+<template>
+   <div class="editor">
+    <editor-menu-bar :editor="editor">
+      <div class="menubar" slot-scope="{ commands, isActive }">
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.bold() }"
+          @click="commands.bold"
+        >
+        <icon name="bold"></icon>
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.italic() }"
+          @click="commands.italic"
+        >
+        <icon name="italic" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.strike() }"
+          @click="commands.strike"
+        >
+          <icon name="strike" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.underline() }"
+          @click="commands.underline"
+        >
+          <icon name="underline" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.code() }"
+          @click="commands.code"
+        >
+          <icon name="code" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.paragraph() }"
+          @click="commands.paragraph"
+        >
+          <icon name="paragraph" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.heading({ level: 1 }) }"
+          @click="commands.heading({ level: 1 })"
+        >
+          H1
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.heading({ level: 2 }) }"
+          @click="commands.heading({ level: 2 })"
+        >
+          H2
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.heading({ level: 3 }) }"
+          @click="commands.heading({ level: 3 })"
+        >
+          H3
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.bullet_list() }"
+          @click="commands.bullet_list"
+        >
+          <icon name="ul" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.ordered_list() }"
+          @click="commands.ordered_list"
+        >
+          <icon name="ol" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.blockquote() }"
+          @click="commands.blockquote"
+        >
+          <icon name="quote" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.code_block() }"
+          @click="commands.code_block"
+        >
+          <icon name="code" />
+        </button>
+
+        <button
+          class="menubar__button"
+          @click="commands.undo"
+        >
+          <icon name="undo" />
+        </button>
+
+        <button
+          class="menubar__button"
+          @click="commands.redo"
+        >
+          <icon name="redo" />
+        </button>
+
+      </div>
+    </editor-menu-bar>
+    <br>
+    <editor-content class="editor__content" :editor="editor" />
+  </div> 
+
+    
+</template>
+
+<script>
+// Import the editor
+import Icon from '@/components/Icon'
+import { Editor, EditorContent, EditorMenuBar} from 'tiptap'
+import {
+  Blockquote,
+  CodeBlock,
+  HardBreak,
+  Heading,
+  OrderedList,
+  BulletList,
+  ListItem,
+  TodoItem,
+  TodoList,
+  Bold,
+  Code,
+  Italic,
+  Link,
+  Strike,
+  Underline,
+  History,
+} from 'tiptap-extensions'
+
+export default {
+    name: "editorTest",
+
+    components: {
+        EditorContent,
+        EditorMenuBar,
+        Icon
+  
+       
+    },
+
+    data() {
+        return {
+            editor: null,
+            
+
+        }
+    },
+    mounted() {
+        this.editor = new Editor({
+            content: '<p>This is just a boring paragraph</p>',
+            autoFocus: true, 
+            extensions: [
+              new Blockquote(),
+              new BulletList(),
+              new CodeBlock(),
+              new HardBreak(),
+              new Heading({ levels: [1, 2, 3] }),
+              new ListItem(),
+              new OrderedList(),
+              new TodoItem(),
+              new TodoList(),
+              new Bold(),
+              new Code(),
+              new Italic(),
+              new Link(),
+              new Strike(),
+              new Underline(),
+              new History(),
+        ]
+
+        })
+    },
+    beforeDestroy() {
+        this.editor.destroy()
+    },
+}
+</script>
+
+<style>
+.editor{
+    outline: none;
+    height: 50vh;
+    width: 50vw;
+    margin: auto;
+    text-align: left;
+    overflow: scroll;
+}
+
+.menubar{
+    text-align: left
+}
+
+
+
+
+</style>
