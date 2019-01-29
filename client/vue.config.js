a39b3d4556260f9e2a6cf04999cffd541b5cce6e
Steven Suh <stevenesuh@gmail.com>
2019-01-29 10:42:04 -0800

    remove google plus from oauth due to service issues

---
+++ client/vue.config.js
@@ -0,0 +1,15 @@
+// reference: https://cli.vuejs.org/config/#baseurl
+module.exports = {
+  /**
+   * This app should be a multi-page because:
+   *  - landing page ('thevinegar.com') should be SEO friendly
+   *    (vue is not SEO friendly since it creates HTML/CSS after vue.js is loaded)
+   */
+  // pages: {
+  //   index: {
+  //     entry: 'src/main.js',
+  //     template: 'public/app.html',
+  //     filename: 'app.html',
+  //   },
+  // },
+};
