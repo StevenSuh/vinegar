613cb3ebcd2209337c0230977fd83a2ccbb6bd4a
Steven Suh <stevenesuh@gmail.com>
2019-03-14 04:46:24 -0700

    deploy zone fix

--- deploy-template.sh
+++ deploy-template.sh
@@ -76,7 +76,7 @@ done
 if [ -z "$DEV" ]; then
   gcloud auth activate-service-account --key-file vinegar-google-credentials.json
   gcloud config set project vinegar
-  gcloud config set compute/zone us-west2-a
+  gcloud config set compute/zone us-west1-a
   gcloud container clusters get-credentials vinegar-prod
 else
   kubectl config use-context minikube
